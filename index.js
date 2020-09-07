1453a67e01f731f8e156423f58cd31ac6d6cc3dd
saehm <saehrimnir@gmail.com>
2020-09-07 11:39:43 +0200

    start

---
+++ index.js
@@ -0,0 +1,9 @@
+export * as FMNIST from "./dataset/fmnist.json";
+export * as IRIS from "./dataset/iris.json";
+//export * as MNIST from "./dataset/mnist.json";
+export * as OECD from "./dataset/oecd.json";
+export * as SPOTIFY from "./dataset/spotify.json";
+export * as SSHAPE from "./dataset/sshape.json";
+export * as SWISSROLL from "./dataset/swissroll.json";
+export * as WAVES from "./dataset/waves.json";
+
---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import { terser } from "rollup-plugin-terser";
+import json from '@rollup/plugin-json';
+import * as meta from "./package.json";
+
+const copyright = `// ${meta.homepage} v${meta.version} Copyright ${(new Date).getFullYear()} ${meta.author.name}`;
+
+export default [{
+    input: "index.js",
+    output: {
+        extend: true,
+        file: "dist/dataset.js",
+        format: "umd",
+        indent: false,
+        name: "datasets"
+    },
+    plugins: [
+        json(),
+    ]
+}, {
+    input: "index.js",
+    output: {
+        extend: true,
+        file: "dist/dataset.min.js",
+        format: "umd",
+        indent: false,
+        name: "datasets"
+    },
+    plugins: [
+        json(), terser({format: {preamble: copyright}})
+    ]
+}]
