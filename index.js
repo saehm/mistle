78a2b76477185633ff165aa96c2f1be4acb10a5b
saehm <saehrimnir@gmail.com>
2020-09-07 12:10:12 +0200

    start

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export * as FMNIST from "./dataset/fmnist.json";
 export * as IRIS from "./dataset/iris.json";
-//export * as MNIST from "./dataset/mnist.json";
+export * as MNIST from "./dataset/mnist.json";
 export * as OECD from "./dataset/oecd.json";
 export * as SPOTIFY from "./dataset/spotify.json";
 export * as SSHAPE from "./dataset/sshape.json";
